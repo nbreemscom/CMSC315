89b3d1576b1d77fc09f93b38be49ece7dd9f7126
nbreemscom <nbreemscom@gmail.com>
2024-10-23 00:56:11 -0500

    Implemented Tree printing

--- Tree/src/tree/Node.java
+++ Tree/src/tree/Node.java
@@ -40,4 +40,64 @@ public class Node<T> {
 				right == null ? -1 : right.getHeight()
 				);
 	}
+	
+	public static void showTrunks(Trunk p)
+    {
+        if (p == null) {
+            return;
+        }
+ 
+        showTrunks(p.prev);
+        System.out.print(p.str);
+    }
+ 
+    public static void printTree(Node subRoot, Trunk prev, boolean isLeft)
+    {
+    	// printTree method sourced from https://www.techiedelight.com/c-program-print-binary-tree/
+    	
+        if (subRoot == null) {
+            return;
+        }
+ 
+        String prev_str = "    ";
+        Trunk trunk = new Trunk(prev, prev_str);
+ 
+        printTree(subRoot.right, trunk, true);
+ 
+        if (prev == null) {
+            trunk.str = "———";
+        }
+        else if (isLeft) {
+            trunk.str = ",———";
+            prev_str = "   |";
+        }
+        else {
+            trunk.str = "`———";
+            prev.str = prev_str;
+        }
+ 
+        showTrunks(trunk);
+        System.out.println(" " + subRoot.element);
+ 
+        if (prev != null) {
+            prev.str = prev_str;
+        }
+        trunk.str = "   |";
+ 
+        printTree(subRoot.left, trunk, false);
+    }
+
 }
+
+/* This class is used for printing the tree */
+class Trunk
+{
+    Trunk prev;
+    String str;
+ 
+    Trunk(Trunk prev, String str)
+    {
+        this.prev = prev;
+        this.str = str;
+    }
+};
--- Tree/src/tree/Tree.java
+++ Tree/src/tree/Tree.java
@@ -11,18 +11,20 @@ public class Tree<T> {
 	public static void main(String[] args) {
 		Tree<String> t = new Tree<String>();
 		
-		t.root = new Node<String>("P", null);
-		t.root.left = new Node<String>("M", t.root);
-		t.root.right = new Node<String>("D", t.root);
-		t.root.left.left = new Node<String>("F", t.root.left);
-		t.root.left.right = new Node<String>("X", t.root.left);
-		t.root.right.right = new Node<String>("N", t.root.right);
-		t.root.right.right.left = new Node<String>("R", t.root.right.right);
-		t.root.right.right.right = new Node<String>("G", t.root.right.right);
+		t.root = new Node<String>("A", null);
+		t.root.left = new Node<String>("B", t.root);
+		t.root.left.right = new Node<String>("D", t.root.left);
+		t.root.right = new Node<String>("C", t.root);
+		t.root.right.left = new Node<String>("E", t.root.right);
+		t.root.right.left.left = new Node<String>("G", t.root.right.left);
+		t.root.right.right = new Node<String>("F", t.root.right);
+		t.root.right.right.left = new Node<String>("H", t.root.right.right);
+		t.root.right.right.right = new Node<String>("I", t.root.right.right);
 		
 		System.out.println("This tree has " + t.getNumLeaves() + " leaves.");
 		System.out.println("This tree has " + t.getNumNodes() + " nodes.");
 		System.out.println("The height of this tree is: " + t.root.getHeight());
+		t.printTree();
 	}
 
 	private int getNumLeaves() {
@@ -33,4 +35,8 @@ public class Tree<T> {
 		return root.getNumNodes();
 	}
 
+	public void printTree() {
+		root.printTree(root, null, false);
+	}
+	
 }
