6c1aae0d55f9d82ef4d551f9c2f25e80a58edeb6
nbreemscom <nbreemscom@gmail.com>
2024-10-28 08:33:41 -0500

    Added a contains() method.

--- Tree/src/tree/Tree.java
+++ Tree/src/tree/Tree.java
@@ -3,26 +3,24 @@ package tree;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Tree<T> {
+public class Tree<T extends Comparable<T>> {
 
 	Node<T> root;
+	int size;
 	
 	public Tree() {
 		root = null;
+		size = 0;
 	}
 	
 	public static void main(String[] args) {
 		Tree<String> t = new Tree<String>();
-		
-		t.root = new Node<String>("A", null);
-		t.root.left = new Node<String>("B", t.root);
-		t.root.left.right = new Node<String>("D", t.root.left);
-		t.root.right = new Node<String>("C", t.root);
-		t.root.right.left = new Node<String>("E", t.root.right);
-		t.root.right.left.left = new Node<String>("G", t.root.right.left);
-		t.root.right.right = new Node<String>("F", t.root.right);
-		t.root.right.right.left = new Node<String>("H", t.root.right.right);
-		t.root.right.right.right = new Node<String>("I", t.root.right.right);
+
+		t.add("C");
+		t.add("A");
+		t.add("D");
+		t.add("B");
+		t.add("E");
 		
 		System.out.println("This tree has " + t.getNumLeaves() + " leaves.");
 		System.out.println("This tree has " + t.getNumNodes() + " nodes.");
@@ -34,6 +32,73 @@ public class Tree<T> {
 		t.postOrderTraversal();
 		t.breadthFirstTraversal();
 		
+	}
+
+	private void clear() {
+		root = null;
+		size = 0;
+	}
+	
+	public boolean contains(T o) {
+		if (root == null)
+			return false;
+		
+		Node<T> cursor = root;
+		while (true) {
+			int c = o.compareTo(cursor.element);
+			if (c == 0) 
+				return true;
+			else if (c < 0) { 
+				if (cursor.left != null) {
+					cursor = cursor.left;
+				} else {
+					return false;
+				}
+			} else { 
+				if (cursor.right != null) {
+					cursor = cursor.right;
+				} else {
+					return false;
+				}
+			}
+		}
+	}
+	
+	
+	public boolean add(T o) {
+		if (root == null) {
+			root = new Node<T>(o, null);
+			size++;
+			return true;
+		}
+		
+		Node<T> cursor = root;
+		while (true) {
+			int c = o.compareTo(cursor.element);
+			if (c == 0) {
+				// Item is already in the tree!
+				return false;
+			} else if (c < 0) {
+				// item is less than the cursor
+				if (cursor.left != null) {
+					cursor = cursor.left;
+				} else {
+					// add item to cursor.left
+					cursor.left = new Node<T>(o, cursor);
+					size++;
+					return true;
+				}
+			} else {
+				// item is greater than the cursor
+				if (cursor.right != null)
+					cursor = cursor.right;
+				else {
+					cursor.right = new Node<T>(o, cursor);
+					size++;
+					return true;
+				}
+			}
+		}	
 	}
 
 	private void breadthFirstTraversal() {
